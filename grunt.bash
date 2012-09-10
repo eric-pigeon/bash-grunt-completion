89f6c732664b5724d8bac9642e14a92ba4031365
Eric Pigeon <eric.r.pigeon@gmail.com>
2012-09-10 00:51:22 -0400

    Added support for flags, built-in tasks, and a better regex

--- grunt.bash
+++ grunt.bash
@@ -1,15 +1,23 @@
 _grunt() {
-	local cur prev opts
+	local cur prev options tasks
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-	# if theres a grunt.js file in the current directory opts match registerTask regex
+	options="--help -h --base --no-color --config --debug -d --force -f --tasks --npm --no-write --verbose -v --version"
+	tasks="concat init lint min qunit server test watch"
+
+	# if theres a grunt.js file in the current directory tasks match registerTask regex
 	if [[ -f ./grunt.js ]]; then
-	  opts=$(sed -En 's/^.*registerTask\(['\''"](.*)['\''"].*$/\1/p' ./grunt.js)
+	  tasks+=" "$(sed -En 's/^.*registerTask\(\s*['\''"](.*)['\''"]\s*,.*$/\1/p' ./grunt.js)
 	fi
 
-  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	if [[ $cur == -* ]]; then
+		COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
+	else
+		COMPREPLY=( $(compgen -W "${tasks}" -- ${cur}) )
+	fi
+
   return 0
 }
 
